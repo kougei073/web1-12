58e73ff00920b288c965d44655f894983958f0d2
kougei073 <g1627073@st.t-kougei.ac.jp>
2017-06-26 17:11:09 +0900

    Update default.js

--- default.js
+++ default.js
@@ -1,6 +1,6 @@
 var answer = parseInt(window.prompt('Fizz-Buss問題　数字を入力してください'));
 
-for(var i=1; i < answer; i++){
+for(var i=1; i <= answer; i++){
 var li = document.createElement('li');
   li.textContent = i;
   document.getElementById('list').appendChild(li);
