1872109c536b81d553d9b6fc78ca74dbebf81038
kougei073 <g1627073@st.t-kougei.ac.jp>
2017-06-26 17:33:20 +0900

    Update default.js

--- default.js
+++ default.js
@@ -4,15 +4,12 @@ for(var i = 1; i <= answer; i++){
 var li = document.createElement('li');
   if(i % 3 === 0 && i % 5 === 0){
  li.textContent = 'FizzBuzz';
- }
- else if(i % 3 === 0){
+ }else if(i % 3 === 0){
  li.textContent = 'Fizz';
- }
-   else if(i % 5 === 0){
+ }else if(i % 5 === 0){
  li.textContent = 'Buzz';
- }
- else{
-  li.textContent = i;
+ }else{
+   li.textContent = i;
  }
   document.getElementById('list').appendChild(li);
 }
