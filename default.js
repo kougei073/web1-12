575be7aee838d36e0f1d6cc627243c7eeb697e6f
kougei073 <g1627073@st.t-kougei.ac.jp>
2017-06-26 18:13:17 +0900

    Update default.js

--- default.js
+++ default.js
@@ -2,7 +2,7 @@ var answer = parseInt(window.prompt('Fizz-Buss問題　数字を入力してく�
 
 for(var i = 1; i <= answer; i++){
 var li = document.createElement('li');
- if(answer < 100　|| answer < 0){
+ if(answer < 100 || answer < 0){
   li.textContent = 'error';
   break;
  }else if(i % 3 === 0 && i % 5 === 0){
